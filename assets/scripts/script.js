9ae3c767b76e9733cd6227ec3c615c3d0a44d5eb
ThereWolf <bdunham484@gmail.com>
2023-11-14 20:05:52 -0600

    intial commit and then some

---
+++ assets/scripts/script.js
@@ -0,0 +1,7 @@
+var startButtonEl = document.getElementById('start-button');
+
+const startGame = function() {
+    console.log('Good Luck');
+}
+
+startButtonEl.addEventListener('click', startGame);
